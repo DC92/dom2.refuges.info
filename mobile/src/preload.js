0db601911e073075681b13bd377cd59bbd2962c9
Dominique Cavailhez <rando.gv92@gmail.com>
2026-01-15 10:19:07 +0100

    indexedDB  / idb-keyval

--- mobile/src/preload.js
+++ mobile/src/preload.js
@@ -2,7 +2,7 @@
  *
  * Les dalles OpenHikingMap sont simplement appelées sans que le résultat ne soit utilisé,
  * elles sont mémorisées par le cache de l'explorateur le temps et l'espace permis par celui-ci
- * Seuls sont mémorisées dans localStorage.preLoadedTiles les date d'expiration,
+ * Seuls sont mémorisées dans indexedDB les date de cache,
  * les informations nécéssaires pour afficher les icônes sur la carte
  * sont chargées globalement par GeoJsonAjaxCluster à chaque modification d'un point. */
 const tilesRefreshTime = 30000, // Milliseconds
@@ -17,37 +17,54 @@ const tilesRefreshTime = 30000, // Milliseconds
  * Les photos des points visualisés sont sont mémorisées par le cache de l'explorateur */
 const pointsTileSize = 0.5; // ° lon / lat
 
+/* global idbKeyval */
+
+/* Accès à la base de données explorateur indexedDB
+   https://www.npmjs.com/package/idb-keyval
+idbKeyval.set('hello', 'World');
+idbKeyval.setMany([
+  [123, 456],
+  ['Bonjour', 'Toi'],
+]);
+idbKeyval.get('hello').then((val) => console.log(val));
+idbKeyval.entries().then((entries) => console.log(entries));
+idbKeyval.keys().then((keys) => console.log(keys));
+idbKeyval.values().then((values) => console.log(values));
+*/
+
 /* eslint-disable no-unused-vars */
 function preload(map, position) {
-  //console.log('preload');
-
   // Preload OpenHikingMap tiles
-  const preLoadedTiles = JSON.parse(localStorage.preLoadedTiles || '{}');
-  let leftToFetch = maxTilesPerRequest,
-    loadedTilesChanged = false;
+  const preLoadedTiles = [];
+  let leftToFetch = maxTilesPerRequest;
 
-  for (let ecart = 1; ecart <= preloadedTilesAround; ecart++)
-    for (let zoom = minZoomPreloadedTiles; zoom <= maxZoomPreloadedTiles; zoom++) {
-      const coordPX = map.project([position.lat, position.lng], zoom),
-        tileX = Math.round(coordPX.x / 256),
-        tileY = Math.round(coordPX.y / 256);
+  // Get aready preloaded tiles dates
+  idbKeyval.entries().then((entries) => {
+    entries.forEach((entry) => {
+      //if(entry[0].toString().includes('/'))
+      preLoadedTiles[entry[0]] = entry[1];
+    });
 
-      for (let x = tileX - ecart; x < tileX + ecart; x++)
-        for (let y = tileY - ecart; y < tileY + ecart; y++) {
-          const baseTileRef = zoom + '/' + x + '/' + y,
-            expirationDate = (preLoadedTiles[baseTileRef] || 0) + tilesRefreshTime,
-            url = 'https://tile.openmaps.fr/openhikingmap/' + baseTileRef + '.png';
+    // When entries are fetched from indexedDB
+    for (let ecart = 1; ecart <= preloadedTilesAround; ecart++)
+      for (let zoom = minZoomPreloadedTiles; zoom <= maxZoomPreloadedTiles; zoom++) {
+        const coordPX = map.project([position.lat, position.lng], zoom),
+          tileX = Math.round(coordPX.x / 256),
+          tileY = Math.round(coordPX.y / 256);
 
-          if (expirationDate < Date.now() && leftToFetch-- > 0) {
-            //console.log(baseTileRef);
-            preLoadedTiles[baseTileRef] = Date.now();
-            fetch(url); // Load the tile on the brother cache (wait for the answer)
-            loadedTilesChanged = true;
+        for (let x = tileX - ecart; x < tileX + ecart; x++)
+          for (let y = tileY - ecart; y < tileY + ecart; y++) {
+            const baseTileRef = zoom + '/' + x + '/' + y,
+              expirationDate = (preLoadedTiles[baseTileRef] || 0) + tilesRefreshTime,
+              url = 'https://tile.openmaps.fr/openhikingmap/' + baseTileRef + '.png';
+
+            if (expirationDate < Date.now() && leftToFetch-- > 0) {
+              idbKeyval.set(baseTileRef, Date.now()); // Mark expiration date
+              fetch(url); // Load the tile on the brother cache (wait for the answer)
+            }
           }
-        }
-    }
-  if (loadedTilesChanged)
-    localStorage.preLoadedTiles = JSON.stringify(preLoadedTiles);
+      }
+  });
 
   // Preload points & commentaires
   const //preLoadedPoints = JSON.parse(localStorage.preLoadedPoints || '{}'),
@@ -69,6 +86,4 @@ function preload(map, position) {
     .catch((error) => {
       console.error('Error: ' + error + ' ' + url);
     });
-
-  //if (loadedPointsChanged) localStorage.preLoadedPoints = JSON.stringify(preLoadedPoints);
 }
