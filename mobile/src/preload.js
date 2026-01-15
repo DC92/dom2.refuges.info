4b8ab4d275e0b4d0f24c256cb202905a3be431b0
Dominique Cavailhez <rando.gv92@gmail.com>
2026-01-15 21:37:52 +0100

    Preload points & commentaires

--- mobile/src/preload.js
+++ mobile/src/preload.js
@@ -18,19 +18,9 @@ const tilesRefreshTime = 30000, // Milliseconds
 const pointsTileSize = 0.5; // ° lon / lat
 
 /* global idbKeyval */
-
 /* Accès à la base de données explorateur indexedDB
-   https://www.npmjs.com/package/idb-keyval
-idbKeyval.set('hello', 'World');
-idbKeyval.setMany([
-  [123, 456],
-  ['Bonjour', 'Toi'],
-]);
-idbKeyval.get('hello').then((val) => console.log(val));
-idbKeyval.entries().then((entries) => console.log(entries));
-idbKeyval.keys().then((keys) => console.log(keys));
-idbKeyval.values().then((values) => console.log(values));
-*/
+ * https://www.npmjs.com/package/idb-keyval
+ */
 
 /* eslint-disable no-unused-vars */
 function preload(map, position) {
@@ -41,19 +31,19 @@ function preload(map, position) {
   // Get aready preloaded tiles dates
   idbKeyval.entries().then((entries) => {
     entries.forEach((entry) => {
-      //if(entry[0].toString().includes('/'))
-      preLoadedTiles[entry[0]] = entry[1];
+      if (entry[0].toString().includes('/'))
+        preLoadedTiles[entry[0]] = entry[1];
     });
 
     // When entries are fetched from indexedDB
     for (let ecart = 1; ecart <= preloadedTilesAround; ecart++)
       for (let zoom = minZoomPreloadedTiles; zoom <= maxZoomPreloadedTiles; zoom++) {
-        const coordPX = map.project([position.lat, position.lng], zoom),
-          tileX = Math.round(coordPX.x / 256),
-          tileY = Math.round(coordPX.y / 256);
+        const baseTileXY = Object.values(
+          map.project(Object.values(position), zoom)
+        ).map(a => Math.round(a / 256));
 
-        for (let x = tileX - ecart; x < tileX + ecart; x++)
-          for (let y = tileY - ecart; y < tileY + ecart; y++) {
+        for (let x = baseTileXY[0] - ecart; x < baseTileXY[0] + ecart; x++)
+          for (let y = baseTileXY[1] - ecart; y < baseTileXY[1] + ecart; y++) {
             const baseTileRef = zoom + '/' + x + '/' + y,
               expirationDate = (preLoadedTiles[baseTileRef] || 0) + tilesRefreshTime,
               url = 'https://tile.openmaps.fr/openhikingmap/' + baseTileRef + '.png';
@@ -67,23 +57,23 @@ function preload(map, position) {
   });
 
   // Preload points & commentaires
-  const //preLoadedPoints = JSON.parse(localStorage.preLoadedPoints || '{}'),
-    tx = Math.round(position.lng / pointsTileSize),
-    ty = Math.round(position.lat / pointsTileSize),
-    bbox = [tx, ty, tx + 1, ty + 1].map(a => Math.round(a * pointsTileSize * 10) / 10),
-    url = window.location.origin + '/api/bbox?detail=complet&nb_points=all&bbox=' + bbox.join(',');
+  const xy = Object.values(position).map(a => Math.round(a / pointsTileSize));
 
-  let loadedPointsChanged = false;
+  for (let x = 0; x < 2; x++)
+    for (let y = 0; y < 2; y++) {
+      const bbox = [xy[1] + y - 1, xy[0] + x - 1, xy[1] + y, xy[0] + x].map(a => a * pointsTileSize),
+        url = window.location.origin + '/api/bbox' +
+        '?detail=complet&nb_points=all&bbox=' + bbox.join(',');
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((json) => {
-      json.features.forEach((feature) => {
-        //console.info(feature);
-        loadedPointsChanged = true;
-      });
-    })
-    .catch((error) => {
-      console.error('Error: ' + error + ' ' + url);
-    });
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+          json.features.forEach((feature) => {
+            idbKeyval.set(feature.id, feature); // Cache point data
+          });
+        })
+        .catch((error) => { //TODO ??? errors pour tout ou rien
+          console.error('Error: ' + error + ' ' + url);
+        });
+    }
 }
