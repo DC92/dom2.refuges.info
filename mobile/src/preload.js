6513be85232abd66dedacce51591cd0e83372d22
Dominique Cavailhez <rando.gv92@gmail.com>
2026-01-18 18:02:31 +0100

    Tentative load points + commentaires

--- mobile/src/preload.js
+++ mobile/src/preload.js
@@ -4,6 +4,36 @@
  * https://www.npmjs.com/package/idb-keyval
  */
 
+/********************************************************************
+ * Les informations nécéssaires pour afficher les icônes sur la carte
+ * sont chargées globalement par GeoJsonAjaxCluster
+ * à chaque modification d'un point.
+ */
+//TODO : le faire ici
+
+/******************************************************************************************
+ * Les informations nécéssaires à l'affichage de la fiche d'un point et de ses commentaires
+ * sont chargées par dalles dans indexedDB avec la clé égale à la valeur de id_point
+ * Une entrée indexedDB est créée, dont la clé est 0.5,43.5,1,44 et la valeur la date de mise en cache
+ * Une fois chargés, ne sont rafraichis que les points ou commentaires récement modifiés.
+ * Les photos des points visualisés sont sont mémorisées par le cache de l'explorateur
+ */
+//const pointsTileSize = 0.25; // ° lon / lat
+const pointsTileSize = 0.1; // ° lon / lat
+
+/************************************************************************
+ * Les dalles OpenHikingMap sont mémorisées par le cache de l'explorateur
+ * le temps et l'espace permis par celui-ci
+ * elles sont simplement appelées sans que le résultat ne soit utilisé.
+ * Une entrée indexedDB est créée, dont la clé est z/x/y et la valeur la date de mise en cache
+ */
+const tilesRefreshTime = 30000, // Milliseconds
+  minZoomPreloadedTiles = 6,
+  maxZoomPreloadedTiles = 15,
+  preloadedTilesAround = 5,
+  maxTilesPerRequest = 40;
+
+
 /* eslint-disable no-unused-vars */
 function preLoad(map, position) {
   const preLoadedEntries = [];
@@ -12,65 +42,61 @@ function preLoad(map, position) {
   /* global idbKeyval */
   idbKeyval.entries().then((entries) => {
     entries.forEach((entry) => {
-      // if (entry[0].toString().includes('/'))
+      //TODO REMOVE ??? if (entry[0].toString().includes('/'))
       preLoadedEntries[entry[0]] = entry[1];
     });
 
-
-    /********************************************************************
-     * Les informations nécéssaires pour afficher les icônes sur la carte
-     * sont chargées globalement par GeoJsonAjaxCluster
-     * à chaque modification d'un point.
-     */
-    //TODO : le faire ici
-
-
-    /******************************************************************************************
-     * Les informations nécéssaires à l'affichage de la fiche d'un point et de ses commentaires
-     * sont chargées par dalles dans indexedDB avec la clé égale à la valeur de id_point
-     * Une entrée indexedDB est créée, dont la clé est 0.5,43.5,1,44 et la valeur la date de mise en cache
-     * Une fois chargés, ne sont rafraichis que les points ou commentaires récement modifiés.
-     * Les photos des points visualisés sont sont mémorisées par le cache de l'explorateur
-     */
-    const pointsTileSize = 0.25, // ° lon / lat
-      xy = Object.values(position).map(a => Math.round(a / pointsTileSize));
+    //***********
+    // Mem points
+    const xy = Object.values(position).map((a) => Math.round(a / pointsTileSize));
 
     for (let x = 0; x < 2; x++)
       for (let y = 0; y < 2; y++) {
-        const bbox = [xy[1] + y - 1, xy[0] + x - 1, xy[1] + y, xy[0] + x].map(a => a * pointsTileSize).join(','),
-          url = window.location.origin + '/api/bbox?detail=complet&nb_points=all&bbox=' + bbox,
-          idPoints = [];
+        const bbox = [xy[1] + y - 1, xy[0] + x - 1, xy[1] + y, xy[0] + x]
+          .map((a) => a * pointsTileSize).join(','),
+          memPairs = []; // Paires à mémoriser
 
-        if (!preLoadedEntries[bbox]) {
-          idbKeyval.set(bbox, Date.now()); // Mark cache date
-          fetch(url)
+        if (!preLoadedEntries[bbox]) { // If not mem in indexedDB
+
+          // Get points data
+          fetch(window.location.origin + '/api/bbox' +
+              '?detail=complet&nb_points=all&bbox=' + bbox)
             .then((response) => response.json())
-            .then((json) => {
-              json.features.forEach((feature) => {
-                idbKeyval.set(feature.id, feature.properties); // Cache point data
-                idPoints.push(feature.id);
+            .then((geoJson) => {
+              if (geoJson.features.length) { // If any point in this area
+                geoJson.features.forEach((feature) => {
+                  feature.properties.commentaires = [];
+                  memPairs[feature.id] = ['P' + feature.id, feature.properties];
+                  //TODO filtrer les valeurs mémorisées
+                });
 
-                //TODO fetch commentaires
-                console.log(idPoints);
-              });
-            }).catch((error) => { //TODO ??? errors pour tout ou rien
-              console.error('Error: ' + error + ' ' + url);
+                // Get commentaires
+                fetch(window.location.origin + '/api/commentaires' +
+                    '?format_texte=html&id_point=' + Object.keys(memPairs).join(','))
+                  .then((response) => response.json())
+                  .then((json) => {
+                    Object.values(json).forEach((commentaire) => {
+                      if (typeof commentaire === 'object')
+                        // Add commentaires to the point data
+                        //TODO filtrer les valeurs mémorisées
+                        if (typeof commentaire === 'object')
+                          memPairs[commentaire.id_point][1]
+                          .commentaires['C' + commentaire.id_commentaire] =
+                          commentaire;
+                    });
+
+                    // Add an entry to idbKeyval to mark this bbox as cached
+                    memPairs.push([bbox, Date.now()]);
+
+                    idbKeyval.setMany(Object.values(memPairs));
+                  });
+              }
             });
         }
       }
 
-    /************************************************************************
-     * Les dalles OpenHikingMap sont mémorisées par le cache de l'explorateur
-     * le temps et l'espace permis par celui-ci
-     * elles sont simplement appelées sans que le résultat ne soit utilisé.
-     * Une entrée indexedDB est créée, dont la clé est z/x/y et la valeur la date de mise en cache
-     */
-    const tilesRefreshTime = 30000, // Milliseconds
-      minZoomPreloadedTiles = 6,
-      maxZoomPreloadedTiles = 15,
-      preloadedTilesAround = 5,
-      maxTilesPerRequest = 40;
-
+    //************************
+    // Mem OpenHikingMap tiles
     let leftToFetch = maxTilesPerRequest;
 
     // Preload OpenHikingMap tiles
@@ -87,7 +113,7 @@ function preLoad(map, position) {
               url = 'https://tile.openmaps.fr/openhikingmap/' + baseTileRef + '.png';
 
             if (cacheDate < Date.now() && leftToFetch-- > 0) {
-              idbKeyval.set(baseTileRef, Date.now()); // Mark cache date
+              //idbKeyval.set(baseTileRef, Date.now()); // Mark cache date
               fetch(url); // Load the tile on the brother cache (wait for the answer)
             }
           }
