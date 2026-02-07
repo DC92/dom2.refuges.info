cc9c873f5d5fa7dff2a7ad3f1071a4a4f4758e8f
Dominique Cavailhez <rando.gv92@gmail.com>
2026-02-07 21:33:15 +0100

    Debug

--- vues/api/points.vue.geojson.php
+++ vues/api/points.vue.geojson.php
@@ -8,7 +8,7 @@ header("Content-Transfer-Encoding: binary");
 headers_cors_par_default();
 headers_cache_api();
 
-$o = [
+$final = [
   "type" => "FeatureCollection",
   "generator" => "Refuges.info API",
   "copyright" => $config_wri["copyright_API"],
@@ -18,22 +18,23 @@ $o = [
 ];
 
 if (isset($config_wri["debug"]))
-  $o["request"] = $_SERVER["REQUEST_URI"];
+  $final["request"] = $_SERVER["REQUEST_URI"];
 
 foreach ($points as $id => $p)
-  $o["features"][] =  [
+  $final["features"][] =  [
     "type" => "Feature",
     "id" => $id,
-    "geometry" => $points_geojson[$id]["geojson"],
+    "geometry" => json_decode($points_geojson[$id]["geojson"]),
     "properties" => $p,
   ];
 
+ksort_recursive($final);
+echo json_encode($final, isset($config_wri["debug"]) ? JSON_PRETTY_PRINT : null);
+
 function ksort_recursive(&$array)
 {
-    if (is_array($array)) {
-        ksort($array);
-        array_walk($array, "ksort_recursive");
-    }
+  if (is_array($array)) {
+    ksort($array);
+    array_walk($array, "ksort_recursive");
+  }
 }
-ksort_recursive($o);
-echo json_encode($o, isset($config_wri["debug"]) ? JSON_PRETTY_PRINT : null);
