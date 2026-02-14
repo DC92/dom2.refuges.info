76fe09c877e37fb1e7c52117eb834c866122ed83
Dominique Cavailhez <rando.gv92@gmail.com>
2026-02-14 08:05:24 +0100

    Reorg cartes & vues

---
+++ mobile/src/carte.js
@@ -0,0 +1,146 @@
+/* global L, appliqueDonnees, preLoadTiles, serveurAPI, defaultPermalink */
+
+/*******************************
+ * Gestion de la carte Leaflet *
+ *******************************/
+
+/*******************
+ * Couches tuilées *
+ *******************/
+const baseLayers = {
+  OpenHikingMap: L.tileLayer('https://tile.openmaps.fr/openhikingmap/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '<a href="https://wiki.openstreetmap.org/wiki/OpenHikingMap">© OpenHikingMap</a>|' +
+      '<a href="https://openmaps.fr/donate">❤️ Donation</a>|' +
+      '<a href="http://www.openstreetmap.org/copyright">© OpenStreetMap</a>|' +
+      '<a target="_blank" href="https://wiki.openstreetmap.org/wiki/OpenHikingMap#Map_Legend">Légende</a>',
+  }),
+  OpenStreetMap: L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy;<a target="_blank" href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>|' +
+      '<a target="_blank" href="https://www.openstreetmap.org/panes/legend">Légende</a>'
+  }),
+
+  // https://geoservices.ign.fr/documentation/services/utilisation-web/extension-pour-leaflet
+  // https://ignf.github.io/geoportal-extensions/leaflet-latest/jsdoc/module-Layers.html#.WMTS
+  /* eslint-disable-next-line new-cap */
+  'Ign plan': L.geoportalLayer.WMTS({
+    layer: 'GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2',
+    format: 'image/png', //TODO BUG format non pris en compte
+    'attribution': 'Orthophotos - Carte © IGN/Geoportail',
+    'maxNativeZoom': 19,
+    'maxZoom': 22,
+  }),
+
+  /* eslint-disable-next-line new-cap */
+  'Ign photo': L.geoportalLayer.WMTS({
+    layer: 'ORTHOIMAGERY.ORTHOPHOTOS',
+  }),
+
+  //TODO https://github.com/plepe/overpass-frontend/blob/master/example-bbox.js
+};
+
+/************************
+ * Couches vectorielles *
+ ************************/
+// Icônes
+const pointsLayer = L.geoJson(null, {
+  // Icônes
+  pointToLayer: (feature, latlng) =>
+    L.marker(latlng, {
+      icon: L.icon({
+        iconUrl: serveurAPI + '/images/icones/' + feature.properties.type.icone + '.svg',
+        size: 24,
+      }),
+    }),
+
+  onEachFeature: (feature, layer) => {
+    // Etiquettes
+    layer.bindTooltip(
+      feature.properties.nom, {
+        permanent: true,
+        direction: 'center',
+      }
+    ).openTooltip();
+
+    // Click
+    layer.on({
+      click: () => {
+        //BEST Fonctions ctrl clic + Apple suivant demande faite à wri github
+        // Affiche les donnés d'entête de la fiche qui sont disponibles dans l'API bbox
+        appliqueDonnees('point', feature.properties);
+
+        // Affiche la page point
+        window.location.hash = 'point=' + feature.properties.id;
+      },
+    });
+  },
+});
+
+// Affichage des clusters et des points
+const clustersLayer = new L.MarkerClusterGroup();
+
+function affichePoints(geoJson) {
+  if (geoJson) {
+    clustersLayer.removeLayer(pointsLayer);
+    pointsLayer.clearLayers();
+    pointsLayer.addData(JSON.parse(geoJson));
+    clustersLayer.addLayer(pointsLayer);
+  }
+}
+
+/******************************
+ * Initialisation de la carte *
+ ******************************/
+const map = L.map('map'),
+  hash = location.hash.replace('#', '').split(','),
+  permalink = hash.length === 3 ? hash :
+  (localStorage.getItem('permalink') || defaultPermalink).split(',');
+
+// Récupére la dernière position
+map.setView(permalink, permalink[2]);
+
+// Layer switcher et contrôles
+Object.values(baseLayers)[0].addTo(map); // Default layer
+L.control.layers(baseLayers).addTo(map);
+
+L.control.scale({
+  imperial: false
+}).addTo(map);
+
+new L.Control.Gps({
+  autoCenter: true,
+}).addTo(map);
+
+new L.Control.Geocoder({
+  position: 'topleft',
+}).addTo(map);
+
+// Couches vectorielles
+clustersLayer.addTo(map);
+affichePoints(localStorage.getItem('points'));
+
+// Charge (ou recharge) les points à afficher
+setTimeout(() => // Attends que les icônes soient chargées
+  fetch(serveurAPI + '/api/points?detail=icones')
+  .then((response) => response.text())
+  .then((geoJson) => {
+    affichePoints(geoJson);
+    localStorage.setItem('points', geoJson);
+  }),
+  100);
+
+map.on('moveend', () => {
+  const pos = map.getCenter(),
+    newPermalink = [pos.lat, pos.lng, map.getZoom()]
+    .map(f => Math.round(f * 1000) / 1000)
+    .join(',');
+
+  // Actualise le permalink
+  localStorage.setItem('permalink', newPermalink);
+  if (hash.length !== 1) //TODO BUG
+    location.hash = newPermalink;
+
+  // Prè-charge les dalles OpenHikingMap, points et commentaires autour de la zone visitée
+  preLoadTiles(map, pos);
+});
---
+++ mobile/src/vues.js
@@ -0,0 +1,116 @@
+/* global requeteAPI, prepareModeleGroupe, appliqueDonnees */
+
+/*************************
+ * Gestion des templates *
+ *************************/
+const nomPages = ['carte', 'point', 'nouvelles'];
+
+// Initialisation de la page lorsque l'URL principale est appelée ou l'ancre change
+function changePage() {
+  const ancre = window.location.hash.replace('#', '').split('=');
+
+  // Attribue le nom de la page à l'ID du body
+  document.body.id = nomPages.includes(ancre[0]) ? ancre[0] : 'carte';
+
+  // Supprime tous les états d'affichage de la page précédente
+  document.body.className = '';
+
+  // Execute la function d'initialisation de la page
+  const nomFonctionAffiche = 'affichePage' + document.body.id.replace(/^[a-z]/u, (m) => m.toUpperCase());
+  window[nomFonctionAffiche](ancre[1]);
+}
+
+window.addEventListener('load', changePage); // Chargement initial du html
+window.addEventListener('popstate', changePage); // L'ancre change ou navigation par les boutons buttons
+
+/**************
+ * Page carte *
+ **************/
+/* eslint-disable-next-line no-unused-vars */
+function affichePageCarte() {}
+
+/******************
+ * Page nouvelles *
+ ******************/
+/* eslint-disable-next-line no-unused-vars */
+function affichePageNouvelles() {
+  //TODO BUG ne précharge pas les nouvelles
+  requeteAPI(
+    'nouvelles',
+    '/api/contributions?format=json&format_texte=html&massif=352&nombre=10',
+    null,
+    (json) => {
+      // Calcule le lien pour afficher la page qui correspond
+      for (const j in json)
+        /* eslint-disable-next-line camelcase */
+        json[j].lien_interne = '#point=' + json[j].id_point;
+
+      prepareModeleGroupe('nouvelles-groupe', Object.keys(json).length - 1); // -1 pour la ligne copyright dans le json
+      appliqueDonnees('nouvelles-groupe', json);
+    }
+  );
+}
+
+/**************
+ * Page point *
+ **************/
+/* eslint-disable-next-line no-unused-vars */
+async function affichePagePoint(idPoint) {
+  /*DCMM
+  const //DCMM infoEl = document.getElementById('infos-point'),
+    //DCMM commentEl = document.getElementById('commentaires'),
+    url = serveurAPI + '/api/points?detail=avec_commentaires&format_texte=html&id=' + idPoint,
+    properties = debugPWA ?
+    await preLoadPoints(url) :
+    await idbKeyval.get(parseInt(idPoint, 10)) || // Si le point est préchargé
+    await preLoadPoints(url); // Essaye de le charger
+
+  map.setView([properties.coord.lat, properties.coord.long], 15);
+  //TODO BUG positionnement carte au chargement de la fiche
+  //TODO charger à partir de la couche bbox points globale.
+  //TODO autres paramètres de page
+
+  Object.entries({
+      ...properties,
+      ...properties.coord,
+      ...properties.type,
+      ...properties.etat
+    })
+    .forEach(entry => {
+      const el = document.getElementById('point-' + entry[0]);
+
+      switch (typeof entry[1]) {
+        case 'string':
+        case 'integer':
+          if (el)
+            el.innerHTML = entry[1];
+          break;
+        case 'object':
+          entry.forEach(subEntry => {
+            if (subEntry.valeur)
+              console.log(subEntry); //DCMM
+            //  console.log(!!subEntry.valeur); //DCMM
+          });
+      }
+    });
+  */
+
+  /*
+  Object.entries(properties.fiche).forEach(entry => {
+//    infoEl.insertAdjacentHTML('beforeend', '<dt>' + entry[0] + ':</dt>');
+  //  infoEl.insertAdjacentHTML('beforeend', '<dl>' + entry[1] + '</dl>');
+  });
+
+  Object.values(properties.commentaires).forEach(c => {
+
+    commentEl.insertAdjacentHTML('beforeend',
+      '<div>' +
+      '<span>' + (c.auteur || 'Inconnu') + ' - ' + (c.date || '') + '</span>' +
+      (c.texte ? '<p>' +
+        (c.photo ? '<img src="' + (serveurAPI + c.photo) + '"></img>' : '') +
+        c.texte + '</p>' : '') +
+      '</div>'
+    );
+  });
+  */
+}
