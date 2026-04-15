cf23e091178c9dccbbc851a08ad468076e4c1e06
Dominique Cavailhez <2606683+Dominique92@users.noreply.github.com>
2026-04-15 07:53:05 +0200

    ISO-Maps

--- mobile/src/carte.js
+++ mobile/src/carte.js
@@ -49,42 +49,49 @@ function tileLayerIGN(paramsIGN, paramsLayer) {
 
 const baseLayers = {
   // Cartes lbres
-  OpenStreetMap: L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxNativeZoom: 19,
-    attribution: '&copy;<a target="_blank" href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>|' +
-      '<a target="_blank" href="https://www.openstreetmap.org/panes/legend">Légende</a>'
-  }),
-  OpenHikingMap: L.tileLayer('https://tile.openmaps.fr/openhikingmap/{z}/{x}/{y}.png', {
-    maxNativeZoom: 18,
-    edgeBufferTiles: 3,
-    attribution: '<a href="https://wiki.openstreetmap.org/wiki/OpenHikingMap">© OpenHikingMap</a>|' +
-      '<a href="https://openmaps.fr/donate">❤️ Donation</a>|' +
-      '<a href="http://www.openstreetmap.org/copyright">© OpenStreetMap</a>|' +
-      '<a target="_blank" href="https://wiki.openstreetmap.org/wiki/OpenHikingMap#Map_Legend">Légende</a>',
-  }),
-  OpenTopoMap: L.tileLayer('https://tile.openmaps.fr/opentopomap/{z}/{x}/{y}.png', {
-    maxNativeZoom: 19,
-    attribution: '<a href="https://github.com/sletuffe/OpenTopoMap">&copy; OTM-R</a> ' +
-      '<a href="https://openmaps.fr/donate">❤️ Donation</a> ' +
-      '<a href="http://www.openstreetmap.org/copyright">&copy; OpenStreetMap</a> ' +
-      '<a target="_blank" href="https://openmaps.fr/map-legend/opentopomap-legend.html">Légende</a>',
-  }),
+    'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxNativeZoom: 19, //TODO revoir zoomS
+      attribution: '&copy;<a target="_blank" href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>|' +
+        '<a target="_blank" href="https://www.openstreetmap.org/panes/legend">Légende</a>'
+    }),
+  OpenHikingMap: L.tileLayer(
+    'https://tile.openmaps.fr/openhikingmap/{z}/{x}/{y}.png', {
+      maxNativeZoom: 18,
+      edgeBufferTiles: 3,
+      attribution: '<a href="https://wiki.openstreetmap.org/wiki/OpenHikingMap">© OpenHikingMap</a>|' +
+        '<a href="https://openmaps.fr/donate">❤️ Donation</a>|' +
+        '<a href="http://www.openstreetmap.org/copyright">© OpenStreetMap</a>|' +
+        '<a target="_blank" href="https://wiki.openstreetmap.org/wiki/OpenHikingMap#Map_Legend">Légende</a>',
+    }),
+  OpenTopoMap: L.tileLayer(
+    'https://tile.openmaps.fr/opentopomap/{z}/{x}/{y}.png', {
+      maxNativeZoom: 19,
+      attribution: '<a href="https://github.com/sletuffe/OpenTopoMap">&copy; OTM-R</a> ' +
+        '<a href="https://openmaps.fr/donate">❤️ Donation</a> ' +
+        '<a href="http://www.openstreetmap.org/copyright">&copy; OpenStreetMap</a> ' +
+        '<a target="_blank" href="https://openmaps.fr/map-legend/opentopomap-legend.html">Légende</a>',
+    }),
+  'ISO-Maps Topo': L.tileLayer(
+    'https://api.iso-maps.com/v1/tiles/{z}/{x}/{y}.webp?api_key=' + mapKeys.isomaps, {
+      maxZoom: 20,
+      attribution: '©<a target="_blank" href="https://www.iso-maps.com/">Isomaps</a> | ' +
+        '©<a target="_blank" href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }),
 
   // Thunderforest
   // https://leaflet-extras.github.io/leaflet-providers/preview/
-  Outdoors: L.tileLayer('https://api.thunderforest.com/outdoors/{z}/{x}/{y}{r}.png?apikey={apikey}', {
-    apikey: mapKeys.thunderforest,
-    attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, ' +
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxNativeZoom: 22,
-  }),
-  //ISO-Maps Topo
-  OpenCycleMap: L.tileLayer('https://api.thunderforest.com/cycle/{z}/{x}/{y}{r}.png?apikey={apikey}', {
-    apikey: mapKeys.thunderforest,
-    maxNativeZoom: 22,
-    attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, ' +
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }),
+  Outdoors: L.tileLayer(
+    'https://api.thunderforest.com/outdoors/{z}/{x}/{y}{r}.png?apikey=' + mapKeys.thunderforest, {
+      attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, ' +
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      maxNativeZoom: 22,
+    }),
+  OpenCycleMap: L.tileLayer(
+    'https://api.thunderforest.com/cycle/{z}/{x}/{y}{r}.png?apikey=' + mapKeys.thunderforest, {
+      maxNativeZoom: 22,
+      attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, ' +
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }),
 
   // IGN
   // https://geoservices.ign.fr/documentation/services/utilisation-web/extension-pour-leaflet
@@ -108,21 +115,27 @@ const baseLayers = {
     format: 'image/png',
   }),
 
-  SwissTopo: L.tileLayer.wms('http://wms.geo.admin.ch/?', {
-    layers: 'ch.swisstopo.pixelkarte-farbe',
-    //layers: 'ch.swisstopo.swissimage',
-    format: 'image/jpeg',
-    //TODO DELETE detectRetina: true,
-  }),
+  SwissTopo: L.tileLayer.wms(
+    'http://wms.geo.admin.ch/?', {
+      layers: 'ch.swisstopo.pixelkarte-farbe',
+      //layers: 'ch.swisstopo.swissimage',
+      format: 'image/jpeg',
+      //TODO DELETE detectRetina: true,
+    }),
   //TODO Autriche
   //TODO Espagne
+
   'Photo Maxar': L.tileLayer.wms(
     'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}@2x.webp?access_token=' + mapKeys.mapbox, {
       maxZoom: 20,
       attribution: '<a href="https://www.mapbox.com/">&copy; Mapbox</a>',
     }),
-  //TODO Photo ArcGis, Google ?
-
+  'Photo Google': L.tileLayer(
+    'http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      maxZoom: 22,
+      attribution: '<a href="https://www.google.com/maps">&copy; Google</a>',
+    }),
   //TODO Couches vectorielles
   //TODO https://github.com/plepe/overpass-frontend/blob/master/example-bbox.js
 };
