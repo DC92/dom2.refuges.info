dd7f2768c6ec576002c75c1f7463c0d01250853a
Dominique Cavailhez <2606683+Dominique92@users.noreply.github.com>
2026-06-02 22:34:02 +0200

    On refusionne bandeau_info

--- forum/ext/RefugesInfo/couplage/event/listener.php
+++ forum/ext/RefugesInfo/couplage/event/listener.php
@@ -82,7 +82,6 @@ class listener implements EventSubscriberInterface
     // pour qu'ils soient insérés aux bons endroits
     ob_start();
     include(fichier_vue('bandeau.html'));
-    include(fichier_vue('bandeau_infos.html'));
     $template->assign_var('BANDEAU', ob_get_clean());
 
     ob_start();
