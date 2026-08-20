e7ed917ce7cf48ce33c602f5e485d7d5e1f23edc
Dominique Cavailhez <2606683+Dominique92@users.noreply.github.com>
2026-08-20 22:12:34 +0200

    Selecteur POI interne/externe

--- vues/index.js
+++ vues/index.js
@@ -15,3 +15,17 @@ const map = initLeafletMap(
 
 controlPreload.addTo(map);
 
+// Passe du sélecteur interne à externe en fonction de la largeur de l'écran
+const selecteurExterneEl = document.getElementById('carte-selecteur'),
+  containerSelecteurPoiEl = document.getElementsByClassName('leaflet-control-layers')[1],
+  listeSelecteurPoiEl = containerSelecteurPoiEl.lastChild;
+
+window.onresize = () => {
+  if (window.innerWidth < 800) {
+    containerSelecteurPoiEl.style.display = 'block';
+    containerSelecteurPoiEl.appendChild(listeSelecteurPoiEl);
+  } else {
+    selecteurExterneEl.appendChild(listeSelecteurPoiEl);
+    containerSelecteurPoiEl.style.display = 'none';
+  }
+};
